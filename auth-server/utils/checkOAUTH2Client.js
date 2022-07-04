f042dcbd3b47f6941f9afd26d646a61a8fd7e574
Aabhas Sao <aabhassao0@gmail.com>
2022-07-04 10:05:05 +0530

    chore: add logs for checkOAUTH2Client
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

--- auth-server/utils/checkOAUTH2Client.js
+++ auth-server/utils/checkOAUTH2Client.js
@@ -1,6 +1,13 @@
 const checkOAUTH2Client = (clients, client_id, client_secret, redirect_uri) => {
   for (let i = 0; i < clients.length; i++) {
     const client = clients[i];
+
+    console.log("Testing checkOAUTH2Client");
+    console.log(client.client_id, client_id);
+    console.log(client.client_secret, client_secret);
+    console.log(client.redirect_uri, redirect_uri);
+    console.log("!!!");
+
     if (
       client.client_id === client_id &&
       client.client_secret === client_secret &&
