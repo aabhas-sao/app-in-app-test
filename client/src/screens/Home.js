222edf4d492118e7cb2ef780503a1851500a9196
Aabhas Sao <aabhassao0@gmail.com>
2022-07-06 22:24:28 +0530

    feat: community login without webview
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

--- client/src/screens/Home.js
+++ client/src/screens/Home.js
@@ -1,27 +1,10 @@
 import React from 'react';
-import { View, Image, Text, StyleSheet } from 'react-native';
+import { View, Image, Text, StyleSheet, SafeAreaView } from 'react-native';
 import ButtonBase from '../components/ui/ButtonBase';
+import WebApp from '../components/WebApp';
 
 const Home = ({ user, setUser }) => {
-  return (
-    <View>
-      <View style={{ width: 100, height: 100, alignSelf: 'center', marginTop: 12 }}>
-        <Image
-          source={{
-            uri: 'https://images.unsplash.com/photo-1574761828925-fe44fd0670d8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2835&q=80',
-          }}
-          style={styles.image}
-        />
-      </View>
-      <Text>Welcome {user.username}</Text>
-      <ButtonBase
-        title="Log out"
-        handlePress={() => setUser(null)}
-        bg={{ backgroundColor: '#D2042D', marginTop: 16 }}
-        color={{ color: '#fff' }}
-      />
-    </View>
-  );
+  return <WebApp />;
 };
 
 const styles = StyleSheet.create({
