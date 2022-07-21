864a177bcf122d61571559a6d68ea5cfcd9f676e
Aabhas Sao <aabhassao0@gmail.com>
2022-07-21 14:20:27 +0530

    refactor: code cleanup
    Signed-off-by: Aabhas Sao <aabhassao0@gmail.com>

--- auth-server/utils/checkOAUTH2Client.js
+++ auth-server/utils/checkOAUTH2Client.js
@@ -5,10 +5,9 @@ const checkOAUTH2Client = async (client_id, client_secret, redirect_uri, clientD
     const client = await Oauth2Client.findOne({ clientId: client_id }).exec();
 
     if (
+      clientDetails.client_id === client_id &&
       client.clientId === client_id &&
       client.clientSecret === client_secret &&
-      clientDetails.client_id === client_id &&
-      clientDetails.client_secret === client_secret
       // checking for redirectURI validation is also a good security measure
       // Turned off here for debugging, in production could turn on this check as well
       // client.redirectURI === redirect_uri
